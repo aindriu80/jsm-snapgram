09915ec01d6e23fdbf367243802ee9e8da562bf7
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-12-15 16:08:39 +0000

    Continuing with the sign up page, loader, initial appwrite

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -14,8 +14,11 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { SignupValidation } from "@/lib/validation";
+import { Loader } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const SignupForm = () => {
+  const isLoading = false;
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
@@ -27,9 +30,8 @@ const SignupForm = () => {
     },
   });
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    // const newUser = await createUserAccount(values)
     console.log(values);
   }
 
@@ -42,7 +44,7 @@ const SignupForm = () => {
             Create a new account
           </h2>
           <p className="text-light-3 small-medium md:base-regular mt-2">
-            To use Snapgram enter your details
+            To use Snapgram, please enter your details
           </p>
 
           <form
@@ -51,21 +53,76 @@ const SignupForm = () => {
           >
             <FormField
               control={form.control}
-              name="username"
+              name="name"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Name</FormLabel>
                   <FormControl>
-                    <Input placeholder="shadcn" {...field} />
+                    <Input type="text" className="shad-input" {...field} />
                   </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
             />
-            <Button type="submit">Submit</Button>
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input type="text" className="shad-input" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="text" className="shad-input" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" className="shad-input" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="shad-button_primary">
+              {isLoading ? (
+                <div className="flex-center gap-2">
+                  <Loader />
+                  Loading...
+                </div>
+              ) : (
+                "Sign Up"
+              )}
+            </Button>
+
+            <p className="text-small-regular text-light-2 text-center mt-2">
+              Already have an account?{" "}
+              <Link
+                to="/sign-in"
+                className="text-primary text-primary-500 text-small-semibold ml-1"
+              >
+                Log in
+              </Link>
+            </p>
           </form>
         </div>
       </Form>
